fd312b38d441f5b9019b2f9d7e5b02b342aeaad7
Tony <tony.kiplagat@student.moringaschool.com>
2023-06-25 11:12:54 +0300

    mini-task lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskInput = document.getElementById("taskInput");
+  const createTaskBtn = document.getElementById("createTaskBtn");
+  const taskList = document.getElementById("taskList");
+
+  function createTask() {
+    const taskDescription = taskInput.value;
+    if (taskDescription) {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskDescription;
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "x";
+      removeBtn.addEventListener("click", () => {
+        taskList.removeChild(taskItem);
+      });
+      taskItem.appendChild(removeBtn);
+      taskList.appendChild(taskItem);
+      taskInput.value = "";
+    }
+  }
+
+  createTaskBtn.addEventListener("click", createTask);
 });
